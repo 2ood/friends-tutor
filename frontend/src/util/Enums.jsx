5fc0b78ded65dba9f0614b92019fc2f86dfba8d4
2ood <kyungmin.official0@gmail.com>
2023-02-21 08:45:32 +0900

    feat : modularRequest

---
+++ frontend/src/util/Enums.jsx
@@ -0,0 +1,15 @@
+const METHOD = {
+    "get" : "get",
+    "post" : "post",
+    "delete" : "delete",
+    "put" : "put"
+};
+
+const HOST = {
+    "address" : "https://jsonplaceholder.typicode.com",
+    "port" : 80
+}
+
+
+
+export {METHOD, HOST};
---
+++ frontend/src/util/ModularRequest.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from 'axios';
+
+import {HOST} from "./Enums";
+
+
+class ModularRequest extends React.Component{
+    constructor(props) {
+    	super(props);
+      
+      	this.state = {
+            path : props.path,
+          	method : props.method,
+            headers : props.headers,
+            body : props.body
+        };
+    }
+
+    async send(){
+        try {
+            const url = `${HOST.address}/${this.props.path}`;
+            //const url = `${HOST.address}:${HOTS.port}/${this.props.path}`;
+            let response = await axios({
+                method : this.props.method,
+                url : url,
+                headers : this.props.headers,
+                data : this.props.body
+            });
+            
+            return response.data; 
+        } catch (e) {
+            console.error("error occured in modularRequest"); 
+            console.error(e.name);
+            console.error(e.message); 
+        } 
+    }
+    
+    render() {
+        return `[${this.props.method}] ${this.props.path}: ${this.props.header} : ${this.props.body}`;
+    }
+}
+
+export default ModularRequest;
