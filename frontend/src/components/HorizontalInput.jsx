5f255232f49d5ce4ec5859e69a73e0a430ceb7d9
2ood <kyungmin.official0@gmail.com>
2023-03-08 13:40:01 +0900

    temporary commit

--- frontend/src/components/HorizontalInput.jsx
+++ frontend/src/components/HorizontalInput.jsx
@@ -1,10 +1,14 @@
+import React, {useState} from "react";
 import * as Styled from "styles/ComponentStyles";
 
 function HorizontalInput(props){
+
+    const [value, setValue] = useState("");
+
     return (
         <Styled.HorizontalInput>
             <label>{props.label}</label>
-            <Styled.NoBorderInput type={props.type} placeholder={props.placeholder} />
+            <Styled.NoBorderInput type={props.type} placeholder={props.placeholder} value={value} onChange= {e=>{setValue(e.target.value)}}/>
         </Styled.HorizontalInput>
     );
 }
--- frontend/src/pages/LectureUploadPage.jsx
+++ frontend/src/pages/LectureUploadPage.jsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import * as Component from "components/Components";
 import * as Styled from "styles/ComponentStyles";
 
 
 
 function LectureUploadPage(){
+
+    const titleRef = useRef();
+    const [note, setNote] = useState("");
+    const [video, setVideo] = useState("");
+    const [desc, setDesc] = useState("");
+    
+
+    function handleSubmit(){
+        let json = {
+            "title" : titleRef.current.value,
+            "note" : note,
+            "video" : video,
+            "desc" : desc
+        };
+        console.log(json);
+    }
+
+    function handleCancel(){
+        
+    }
+
     return (<>
         <Component.Topbar />
         <Styled.MainBodyFrame gap="30px">
             <Styled.ThemedTitle>Upload Lecture</Styled.ThemedTitle>
-            <Component.HorizontalInput type="text" placeholder="Enter your title" label="Title"></Component.HorizontalInput>
-            <Component.HorizontalInput type="url" placeholder="Enter a google drive link" label="Note"></Component.HorizontalInput>
-            <Component.HorizontalInput type="url" placeholder="Enter a youtube link" label="Video"></Component.HorizontalInput>
-            <Styled.ThemedTextarea size="100%" placeholder="Enter an explanation"></Styled.ThemedTextarea>
+            <Component.HorizontalInput type="text" placeholder="Enter your title" label="Title" ref={titleRef}></Component.HorizontalInput>
+            <Component.HorizontalInput type="url" placeholder="Enter a google drive link" label="Note" onChange={e=>{setNote(e.target.value)}}></Component.HorizontalInput>
+            <Component.HorizontalInput type="url" placeholder="Enter a youtube link" label="Video" onChange={e=>{setVideo(e.target.value)}}></Component.HorizontalInput>
+            <Styled.ThemedTextarea size="100%" placeholder="Enter an explanation" defaultvalue="" onChange={e=>{setDesc(e.target.value)}}></Styled.ThemedTextarea>
             <Styled.Buttongroup>
-                <Styled.ThemedButton size="10px" theme="primary">submit</Styled.ThemedButton>
-                <Styled.ThemedButton size="10px" theme="accent">cancel</Styled.ThemedButton>
+                <Styled.ThemedButton size="10px" theme="primary" onClick={handleSubmit}>submit</Styled.ThemedButton>
+                <Styled.ThemedButton size="10px" theme="accent" onClick={handleCancel}>cancel</Styled.ThemedButton>
                 
             </Styled.Buttongroup>
             <Styled.ThemedButton size="50px" theme="secondary">Check the video subtitles here! 🔽</Styled.ThemedButton>
