7a87cd2a92f53e209b18e99559e5a8a3da5980c4
2ood <kyungmin.official0@gmail.com>
2023-03-08 16:55:22 +0900

    feat(viewport) : made static viewport(#21)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,13 @@ import AxiosTest from './util/AxiosTest';
 import LoginTest from './util/LoginTest';
 import TestPage from './util/TestPage';
 import * as Pages from "pages/Pages";
+import top_notch from "img/top_safezone.png";
+import bottom_notch from "img/bottom_safezone.png";
 
 function App() {
   return (
-      <div className='App'>
+      <div className='App viewport-mode'>
+			<img id="top_notch" alt="top_safezone" src={top_notch}/>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/axios-test" element={<AxiosTest />}></Route>
@@ -15,11 +18,12 @@ function App() {
 					<Route path="/certificate" element={<Pages.CertificatePage />}></Route>
 					<Route path="/lecture/upload" element={<Pages.LectureUploadPage />}></Route>
 					<Route path="/lecture/view" element={<Pages.LectureViewPage/>}></Route>
-          <Route path="/lecture/list" element={<Pages.LectureRoomPage/>}></Route>
-          <Route path="/mypage" element={<Pages.Mypage/>}></Route>
+          			<Route path="/lecture/list" element={<Pages.LectureRoomPage/>}></Route>
+          			<Route path="/mypage" element={<Pages.Mypage/>}></Route>
 					<Route path="/" element={<TestPage />}></Route>
 				</Routes>
 			</BrowserRouter>
+			<img id="bottom_notch" alt="top_safezone" src={bottom_notch}/>
 		</div>
   );
 }
