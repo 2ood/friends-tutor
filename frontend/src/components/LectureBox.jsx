c97e7e35e6c8733d9c5ba5440c14c721caba80eb
2ood <kyungmin.official0@gmail.com>
2023-03-14 16:17:41 +0900

    feat(page) : implement axios in lectur room page(#38)

--- frontend/src/components/LectureBox.jsx
+++ frontend/src/components/LectureBox.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import * as Styled from "styles/ComponentStyles";
+import { useNavigate } from "react-router-dom";
 
 function LectureBox(props){
-
-    const videoId = props.videoId??"P1ww1IXRfTA";
+    const navigate = useNavigate();
+    const videoId = props.src.video_id??"P1ww1IXRfTA";
     const imgUrl = `http://img.youtube.com/vi/${videoId}/0.jpg`;
 
     useEffect(()=>{
@@ -11,16 +12,16 @@ function LectureBox(props){
     },[]);
 
     function handleLectureClick(){
-        console.log("Lecture box clicked");
+        navigate(`/lecture/view/${props.src.course_id}`);
     };
 
     return (
         <Styled.LectureBoxFrame width={props.width} onClick={handleLectureClick}>
             <img alt="lecture-thumb" src={imgUrl}/>
-            <Styled.LectureTitle>{props.title??"Lecture Title"}</Styled.LectureTitle>
+            <Styled.LectureTitle>{props.src.title??"Lecture Title"}</Styled.LectureTitle>
             <Styled.LectureDescBox>
-                <Styled.Lecturer>{props.lecturer??"Lecturer name"}</Styled.Lecturer>
-                <Styled.LectureLikes>Likes {props.likes??"0"}</Styled.LectureLikes>
+                <Styled.Lecturer>{props.src.writer??"Lecturer name"}</Styled.Lecturer>
+                <Styled.LectureLikes>Likes {props.src.likes??"0"}</Styled.LectureLikes>
             </Styled.LectureDescBox>
         </Styled.LectureBoxFrame>
     );
