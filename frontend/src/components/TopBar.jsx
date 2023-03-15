2ba322fb39bb25cc3fbbff0bc4c4547ff0c40411
2ood <kyungmin.official0@gmail.com>
2023-03-15 19:39:02 +0900

    refactor(minor) : implemented FE_PATH(#49)

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import * as Styled from "styles/ComponentStyles";
 import home from "img/home.png";
 import profile from "img/profile.png";
+import {FE_PATH} from "util/Enums";
 
 
 
@@ -10,13 +11,13 @@ function Topbar(){
     
     return (
         <Styled.Topbar>
-            <div id="logo" onClick={()=>{navigate("/course/list")}}>FriendsMentor</div>
+            <div id="logo" onClick={()=>{navigate(FE_PATH.course.list)}}>FriendsMentor</div>
             <div id="navgroup">
-                <Styled.NavButton id="nav-home" onClick={()=>{navigate("/course/list")}}>
+                <Styled.NavButton id="nav-home" onClick={()=>{navigate(FE_PATH.course.list)}}>
                     <img src={home} alt="home"></img>
                 </Styled.NavButton>
                 <Styled.NavButton id="nav-profile">
-                    <img src={profile} alt="mypage" onClick={()=>{navigate("/mypage")}}></img>
+                    <img src={profile} alt="mypage" onClick={()=>{navigate(FE_PATH.mypage.mypage)}}></img>
                 </Styled.NavButton>
             </div>
         </Styled.Topbar>
