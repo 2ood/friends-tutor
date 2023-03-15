4a0b5c030496922fa905162d752989675627a6db
2ood <kyungmin.official0@gmail.com>
2023-03-15 18:07:57 +0900

    refactor(misc) : moved test pages and added enums(#47)

--- frontend/src/components/LecturePlayer.jsx
+++ frontend/src/components/LecturePlayer.jsx
@@ -1,9 +1,12 @@
 import * as Styled from "styles/ComponentStyles";
+import blackboard from "img/blackboard-compressed.jpg";
 
 function LecturePlayer(props){
     const src = `https://www.youtube.com/embed/${props.video_id}`;
     return (
-        <Styled.LecturePlayer src={src} title="Youtube Video Player" allow="accelerometer; autoplay; fullscreen; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></Styled.LecturePlayer>
+        props.video_id
+        ?<Styled.LecturePlayer src={src} title="Youtube Video Player" allow="accelerometer; autoplay; fullscreen; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></Styled.LecturePlayer>
+        :<img alt="lecture-thumb" src={blackboard}/>
     )
 }
 
