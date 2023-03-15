7c362b66cc3d9eff480f0a4368f83997b330fe22
2ood <kyungmin.official0@gmail.com>
2023-03-15 20:28:04 +0900

    bugfix(navigation) : /course/view/:id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
 					<Route path={FE_PATH.auth.signin} element={<Pages.SignInPage />}></Route>
 					<Route path={FE_PATH.auth.signup} element={<Pages.SignUpPage />}></Route>
 					<Route path={FE_PATH.course.upload} element={<Pages.LectureUploadPage />}></Route>
-					<Route path={`${FE_PATH.course.view}:id`} element={<Pages.LectureViewPage/>}></Route>
+					<Route path={`${FE_PATH.course.view}/:id`} element={<Pages.LectureViewPage/>}></Route>
 					<Route path={FE_PATH.course.list} element={<Pages.LectureRoomPage/>}></Route>
 					<Route path={FE_PATH.mypage.certificate} element={<Pages.CertificatePage />}></Route>
 					<Route path={FE_PATH.mypage.mypage} element={<Pages.Mypage/>}></Route>
