accd943b4e00da5c8d30f2cd3164f85d4c9f1239
2ood <kyungmin.official0@gmail.com>
2023-03-15 20:20:22 +0900

    refactor(page) : app.js(#49)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AxiosTest from 'pages/AxiosTestPage';
-import LoginTest from 'pages/LoginTestPage';
+import AxiosTestPage from 'pages/AxiosTestPage';
+import LoginTestPage from 'pages/LoginTestPage';
 import TestPage from 'pages/TestPage';
 import * as Pages from "pages/Pages";
-
+import {FE_PATH} from "util/Enums";
 
 
 function App() {
@@ -13,17 +13,17 @@ function App() {
       <div className="App">
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Pages.MainPage />}></Route>
-					<Route path="/sign-in" element={<Pages.SignInPage />}></Route>
-					<Route path="/sign-up" element={<Pages.SignUpPage />}></Route>
-					<Route path="/certificate" element={<Pages.CertificatePage />}></Route>
-					<Route path="/course/upload" element={<Pages.LectureUploadPage />}></Route>
-					<Route path="/course/view/:id" element={<Pages.LectureViewPage/>}></Route>
-					<Route path="/course/list" element={<Pages.LectureRoomPage/>}></Route>
-					<Route path="/mypage" element={<Pages.Mypage/>}></Route>
+					<Route path={FE_PATH.main} element={<Pages.MainPage />}></Route>
+					<Route path={FE_PATH.auth.signin} element={<Pages.SignInPage />}></Route>
+					<Route path={FE_PATH.auth.signup} element={<Pages.SignUpPage />}></Route>
+					<Route path={FE_PATH.course.upload} element={<Pages.LectureUploadPage />}></Route>
+					<Route path={`${FE_PATH.course.view}:id`} element={<Pages.LectureViewPage/>}></Route>
+					<Route path={FE_PATH.course.list} element={<Pages.LectureRoomPage/>}></Route>
+					<Route path={FE_PATH.mypage.certificate} element={<Pages.CertificatePage />}></Route>
+					<Route path={FE_PATH.mypage.mypage} element={<Pages.Mypage/>}></Route>
 					<Route path="/overview" element={<TestPage />}></Route>
-					<Route path="/axios-test" element={<AxiosTest />}></Route>
-					<Route path="/login-test" element={<LoginTest />}></Route>
+					<Route path="/axios-test" element={<AxiosTestPage />}></Route>
+					<Route path="/login-test" element={<LoginTestPage />}></Route>
 				</Routes>
 			</BrowserRouter>
 		</div>
