6295911b91fa5dcb849daba54d8ac80ca6bf16a1
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-03-21 19:25:02 +0900

    feat(language) : language change

--- frontend/src/components/GradeSelect.jsx
+++ frontend/src/components/GradeSelect.jsx
@@ -40,15 +40,15 @@ function GradeSelect(props){
     return (<>
     <Styled.GradeSelect name="choice" value={props.contents.target} onChange={(e)=>{props.contents.set(e.target.value);}}>
             <option default value="" disabled > Enter your grade </option>
-            <option value="4">🖼️초등학교 4학년</option>
-            <option value="5">🖼️초등학교 5학년</option>
-            <option value="6">🖼️초등학교 6학년</option>
-            <option value="7">📙중학교 1학년</option>
-            <option value="8">📙중학교 2학년</option>
-            <option value="9">📙중학교 3학년</option>
-            <option value="10">📚고등학교 1학년</option>
-            <option value="11">📚고등학교 2학년</option>
-            <option value="12">📚고등학교 3학년</option>
+            <option value="4">{four}</option>
+            <option value="5">{five}</option>
+            <option value="6">{six}</option>
+            <option value="7">{seven}</option>
+            <option value="8">{eight}</option>
+            <option value="9">{nine}</option>
+            <option value="10">{ten}</option>
+            <option value="11">{eleven}</option>
+            <option value="12">{twelve}</option>
         </Styled.GradeSelect> 
     </>
         
